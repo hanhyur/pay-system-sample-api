29480c2e1a93956037acb99919ae26b702d94d47
hanhyur <zeidgame@gmail.com>
2023-01-01 14:18:13 +0900

    feat : 메인 클래스 생성
    <description>
    [+]
    >> 실행 클래스 생성

---
+++ pay-system-interface/src/main/java/me/hanhyur/pay/PaySystemApplication.java
@@ -0,0 +1,13 @@
+package me.hanhyur.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaySystemApplication.class, args);
+    }
+
+}
